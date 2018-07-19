e50c0409dbfdde360a8b00c902c967cf23fed8d2
KEVIN D Steele <kevindsteeleii@gmail.com>
2018-07-19 04:17:02 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,8 +1,8 @@
 def display_board (board)
   puts 
-    " #{board[]} | #{board[]} | #{board[]} "
+    " #{board[0]} | #{board[1]} | #{board[2]} "
     +"-----------"
-    +" #{board[]} | #{board[]} | #{board[]} "
+    +" #{board[3]} | #{board[4]} | #{board[5]} "
     +"-----------"
-    +" #{board[]} | #{board[]} | #{board[]} "
+    +" #{board[6]} | #{board[7]} | #{board[8]} "
 end
